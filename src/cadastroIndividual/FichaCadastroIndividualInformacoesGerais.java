221b84148365b57c48dad60dc10c8811758abf80
Otávio Vicente Ribeiro <otavio.ribeiro@freire.com.br>
2016-03-07 11:53:58 -0300

    Atualizando.

---
+++ src/cadastroIndividual/FichaCadastroIndividualInformacoesGerais.java
@@ -0,0 +1,5 @@
+package cadastroIndividual;
+
+public class FichaCadastroIndividualInformacoesGerais {
+
+}
---
+++ src/exemplosThrift/testeProcedimentoMaker.java
@@ -0,0 +1,27 @@
+package exemplosThrift;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import procedimentos.exemplosFichas.ExemploFichaProcedimentoInformacoesGerais;
+
+public class testeProcedimentoMaker {
+	
+	static List<String> dadoXML;
+
+	public static void main(String[] args) {
+		
+		dadoXML = new ArrayList<String>();
+		for (String string : dadoXML) {
+			
+		}
+		
+		InformacoesEnvioDto informacoesEnvioDto = new InformacoesEnvioDto();
+		ExemploFichaProcedimentoInformacoesGerais fichaProcedimento = new ExemploFichaProcedimentoInformacoesGerais();
+		
+	}
+	
+	public void atualizaInformacoesGerais(ExemploFichaProcedimentoInformacoesGerais fichaProcedimento) {
+	}
+
+}
